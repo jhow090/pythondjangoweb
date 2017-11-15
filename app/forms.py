9293ba043c33b03dcffc69cc3278cc86f00bd2c8
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-15 20:03:24 -0200

    Rodrigo

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.forms import ModelForm
-from django.contrib.auth.models import User
 from models import *
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
