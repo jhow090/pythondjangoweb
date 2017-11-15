be6d885dc3d58aa958e5e513002f0cc3255920bd
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-15 18:16:28 -0200

    Rodrigo

--- app/views.py
+++ app/views.py
@@ -38,7 +38,11 @@ def listar_aluno(request):
     )
 
 def novo_aluno(request, template_name='app/aluno/novo_aluno.html'):
-    cursos = Curso.objects.all()
+    assert isinstance(request, HttpRequest)
+    context_instance = RequestContext(request,
+    {
+        'cursos': Curso.objects.all(),
+    })
     form = AlunoForm(request.POST or None)
     if form.is_valid():
         form.save()
