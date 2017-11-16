df4452e77ff29dc2c64df9203055ed2553d2df45
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-16 17:20:08 -0200

    Rodrigo

--- app/forms.py
+++ app/forms.py
@@ -270,60 +270,59 @@ class DisciplinofertadaForm(forms.ModelForm):
                     'semestre_disciplina']
 
 class GradeForm(forms.ModelForm):
-        ano_grade = forms.CharField(
-        max_length=4,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Ano',
-                'type': 'number',
-                'name': 'ano_grade',
-                'id': 'ano_grade',
-                'size': 5,
-                'placeholder': 'Ano'
-            }
-        )
+    ano_grade = forms.CharField(
+    max_length=4,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Ano',
+            'type': 'number',
+            'name': 'ano_grade',
+            'id': 'ano_grade',
+            'size': 5,
+            'placeholder': 'Ano'
+        }
     )
+)
 
-        semestre_grade = forms.CharField(
-        max_length=1,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Semestre',
-                'type': 'number',
-                'name': 'semestre_grade',
-                'id': 'semestre_grade',
-                'size': 5,
-                'placeholder': 'Semestre'
-            }
-        )
+    semestre_grade = forms.CharField(
+    max_length=1,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Semestre',
+            'type': 'number',
+            'name': 'semestre_grade',
+            'id': 'semestre_grade',
+            'size': 5,
+            'placeholder': 'Semestre'
+        }
     )
-        class Meta:
-            model = Grade
-            fields =    ['sigla_curso',
-                        'ano_grade',
-                        'semestre_grade']
+)
+    class Meta:
+        model = Grade
+        fields = [  'sigla_curso',
+                    'ano_grade',
+                    'semestre_grade']
 
 class PeriodoForm(forms.ModelForm):
-        numero_periodo = forms.CharField(
-        max_length=4,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Numero',
-                'type': 'number',
-                'name': 'numero_periodo',
-                'id': 'numero_periodo',
-                'size': 5,
-                'placeholder': 'Numero'
-            }
-        )
+    numero_periodo = forms.CharField(
+    max_length=4,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Numero',
+            'type': 'number',
+            'name': 'numero_periodo',
+            'id': 'numero_periodo',
+            'size': 5,
+            'placeholder': 'Numero'
+        }
     )
-
-        class Meta:
-            model = Periodo
-            fields =    ['sigla_curso',
-                        'ano_grade',
-                        'semestre_grade',
-                        'numero_periodo']
+)
+    class Meta:
+        model = Periodo
+        fields = [  'sigla_curso',
+                    'ano_grade',
+                    'semestre_grade',
+                    'numero_periodo']
 
 class PerioddisciplinaForm(forms.ModelForm):
     class Meta:
@@ -336,100 +335,96 @@ class PerioddisciplinaForm(forms.ModelForm):
                     'nome_disciplina']
 
 class ProfessorForm(forms.ModelForm):
-        ra_professor = forms.CharField(
-        max_length=10,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Ra',
-                'type': 'text',
-                'name': 'ra_professor',
-                'id': 'ra_professor',
-                'size': 5,
-                'placeholder': 'Ra'
-            }
-        )
+    ra_professor = forms.CharField(
+    max_length=10,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Ra',
+            'type': 'text',
+            'name': 'ra_professor',
+            'id': 'ra_professor',
+            'size': 5,
+            'placeholder': 'Ra'
+        }
     )
-
-        apelido_professor = forms.CharField(
-        max_length=30,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Apelido',
-                'type': 'text',
-                'name': 'apelido_professor',
-                'id': 'apelido_professor',
-                'size': 10,
-                'placeholder': 'Apelido'
-            }
-        )
+)
+    apelido_professor = forms.CharField(
+    max_length=30,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Apelido',
+            'type': 'text',
+            'name': 'apelido_professor',
+            'id': 'apelido_professor',
+            'size': 10,
+            'placeholder': 'Apelido'
+        }
     )
-
-        nome_professor = forms.CharField(
-        max_length=120,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Nome',
-                'type': 'text',
-                'name': 'nome_professor',
-                'id': 'nome_professor',
-                'size': 15,
-                'placeholder': 'Nome'
-            }
-        )
+)
+    nome_professor = forms.CharField(
+    max_length=120,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Nome',
+            'type': 'text',
+            'name': 'nome_professor',
+            'id': 'nome_professor',
+            'size': 15,
+            'placeholder': 'Nome'
+        }
     )
-
-        email_professor = forms.CharField(
-        max_length=120,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Email',
-                'type': 'text',
-                'name': 'email_professor',
-                'id': 'email_professor',
-                'size': 10,
-                'placeholder': 'Email'
-            }
-        )
+)
+    email_professor = forms.CharField(
+    max_length=120,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Email',
+            'type': 'text',
+            'name': 'email_professor',
+            'id': 'email_professor',
+            'size': 10,
+            'placeholder': 'Email'
+        }
     )
-
-        celular_professor = forms.CharField(
-        max_length=120,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Celular',
-                'type': 'text',
-                'name': 'celular_professor',
-                'id': 'celular_professor',
-                'size': 10,
-                'placeholder': 'Celular'
-            }
-        )
+)
+    celular_professor = forms.CharField(
+    max_length=120,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Celular',
+            'type': 'text',
+            'name': 'celular_professor',
+            'id': 'celular_professor',
+            'size': 10,
+            'placeholder': 'Celular'
+        }
     )
-        class Meta:
-            model = Professor
-            fields =    ['ra_professor',
-                        'apelido_professor',
-                        'nome_professor',
-                        'email_professor',
-                        'celular_professor']
+)
+    class Meta:
+        model = Professor
+        fields =[   'ra_professor',
+                    'apelido_professor',
+                    'nome_professor',
+                    'email_professor',
+                    'celular_professor']
 
 class TurmaForm(forms.ModelForm):
-        turno_turma = forms.CharField(
-        max_length=15,
-        widget=forms.TextInput(
-            attrs={
-                'title': 'Turma',
-                'type': 'text',
-                'name': 'turno_turma',
-                'id': 'turno_turma',
-                'size': 5,
-                'placeholder': 'Turma'
-            }
-        )
+    turno_turma = forms.CharField(
+    max_length=15,
+    widget=forms.TextInput(
+        attrs={
+            'title': 'Turma',
+            'type': 'text',
+            'name': 'turno_turma',
+            'id': 'turno_turma',
+            'size': 5,
+            'placeholder': 'Turma'
+        }
     )
+)
     class Meta:
         model = Turma
-        fields =    ['nome_disciplina',
+        fields = [  'nome_disciplina',
                     'ano_disciplina',
                     'semestre_disciplina',
                     'turno_turma',
